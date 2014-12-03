6d36281976a4649fb9a7e65e521e60c920b164b5
RonaldPeterson <ronald.peterson@gmail.com>
2014-12-03 10:04:29 +0100

    My first maven project

--- src/main/java/nl/testcode/Main.java
+++ src/main/java/nl/testcode/Main.java
@@ -1,9 +1,11 @@
+package nl.testcode;
+
 /**
  * Created by rpeterson on 3-12-2014.
  */
-public class HelloWorld {
+public class Main {
 
     public static void main(String... args) {
-        System.out.println("HelloWorld.main");
+
     }
 }
---
+++ src/test/java/nl/testcode/MainTest.java
@@ -0,0 +1,17 @@
+package nl.testcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by rpeterson on 3-12-2014.
+ */
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue(Boolean.TRUE);
+
+    }
+}
